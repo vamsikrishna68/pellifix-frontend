da5bd957ae30fe93185120953334761fcce2f75b
AppsWise Technologies Pvt. Ltd <sabareesh.addepalli@appswise.com>
2022-08-09 21:41:13 +0530

    super admin changes

--- src/pages/Layout/Layout.js
+++ src/pages/Layout/Layout.js
@@ -132,6 +132,7 @@ const Layout = () => {
     const navigate = useNavigate();
     const [open, setOpen] = React.useState(false);
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const role='SUPER_ADMIN'
 
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
@@ -150,13 +151,17 @@ const Layout = () => {
     }
 
     const sideMenu = [
-        { title: 'Home', icon: 'https://cdn.lordicon.com/gmzxduhd.json', path: 'home' },
-        { title: 'Whist List', icon: 'https://cdn.lordicon.com/rjzlnunf.json', path: 'wishlist' },
-        { title: 'Chat', icon: 'https://cdn.lordicon.com/zpxybbhl.json', path: 'chat' },
-        { title: 'Edit Profile', icon: 'https://cdn.lordicon.com/wloilxuq.json', path: 'edit-profile' },
-        { title: 'Edit Preference', icon: 'https://cdn.lordicon.com/sbiheqdr.json', path: 'edit-preference' },
-        { title: 'Compare Profiles', icon: 'https://cdn.lordicon.com/soseozvi.json', path: 'compare-profiles' },
-        { title: 'Profile Viewed', icon: 'https://cdn.lordicon.com/tyounuzx.json', path: 'profile-viewed' },
+        { title: 'Home', icon: 'https://cdn.lordicon.com/gmzxduhd.json', path: 'home',role:'USER' },
+        { title: 'Whist List', icon: 'https://cdn.lordicon.com/rjzlnunf.json', path: 'wishlist',role:'USER'  },
+        { title: 'Chat', icon: 'https://cdn.lordicon.com/zpxybbhl.json', path: 'chat',role:'USER'  },
+        { title: 'Edit Profile', icon: 'https://cdn.lordicon.com/wloilxuq.json', path: 'edit-profile',role:'USER'  },
+        { title: 'Edit Preference', icon: 'https://cdn.lordicon.com/sbiheqdr.json', path: 'edit-preference',role:'USER'  },
+        { title: 'Compare Profiles', icon: 'https://cdn.lordicon.com/soseozvi.json', path: 'compare-profiles',role:'USER'  },
+        { title: 'Profile Viewed', icon: 'https://cdn.lordicon.com/tyounuzx.json', path: 'profile-viewed',role:'USER'  },
+        { title: 'Admin Dashboard', icon: 'https://cdn.lordicon.com/imamsnbq.json', path: 'admin-dashboard',role:'SUPER_ADMIN'  },
+        { title: 'Sub-Ordinates', icon: 'https://cdn.lordicon.com/eszyyflr.json', path: 'sub-ordinates',role:'SUPER_ADMIN' },
+        { title: 'Associates', icon: 'https://cdn.lordicon.com/imamsnbq.json', path: 'associates',role:'SUPER_ADMIN'  },
+
     ]
 
     const handleDrawerOpen = () => {
@@ -220,31 +225,34 @@ const Layout = () => {
                 <Divider />
                 <List sx={{ paddingTop: 0 }}>
                     {sideMenu.map((menu, index) => (
-                        <NavLink to={menu.path} className={({ isActive }) =>
-                            isActive ? 'activeRoute' : 'routes'
-                        }
-                        >
-                            <ListItem key={menu.title} disablePadding sx={{ display: 'block' }}>
-                                <ListItemButton
-                                    sx={{
-                                        minHeight: 48,
-                                        justifyContent: open ? 'initial' : 'center',
-                                        padding:0
-                                    }}
-                                >
-                                    <Tooltip title={menu.title} placement="right"> 
-                                        <lord-icon
-                                            src={menu.icon}
-                                            trigger="loop"
-                                            colors="primary:#121331,secondary:#d53833"
-                                            style={{ width: open?'60px':'300px'}}>
-                                        </lord-icon>
-                                  </Tooltip> 
-                                   
-                                    <ListItemText className='menu-text' primary={menu.title} sx={{ opacity: open ? 1 : 0 }} />
-                                </ListItemButton>
-                            </ListItem>
-                        </NavLink>
+
+                        menu.role === 'SUPER_ADMIN' ?
+                            <NavLink to={menu.path} className={({ isActive }) =>
+                                isActive ? 'activeRoute' : 'routes'
+                            }
+                            >
+                                <ListItem key={menu.title} disablePadding sx={{ display: 'block' }}>
+                                    <ListItemButton
+                                        sx={{
+                                            minHeight: 48,
+                                            justifyContent: open ? 'initial' : 'center',
+                                            padding: 0
+                                        }}
+                                    >
+                                        <Tooltip title={menu.title} placement="right">
+                                            <lord-icon
+                                                src={menu.icon}
+                                                trigger="loop"
+                                                colors="primary:#121331,secondary:#d53833"
+                                                style={{ width: open ? '60px' : '500px', marginLeft: open ? 'auto' : '10px'  }}>
+                                            </lord-icon>
+                                        </Tooltip>
+
+                                        <ListItemText className='menu-text' primary={menu.title} sx={{ opacity: open ? 1 : 0 }} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </NavLink> : null
+
                     ))}
                 </List>
 
